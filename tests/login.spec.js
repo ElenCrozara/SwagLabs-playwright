41cb0d5f1f55d3a7559a829b3a2b15446a49333e
elen <elen.crozzara@gmail.com>
2024-02-15 22:30:43 -0300

    Laughing error scenario

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,3 +12,13 @@ test('the user login with succes', async ({page}) => {
     await expect(productTitle).toHaveText('Products')
     await page.pause()
 });
+
+
+test('the user inserts wrong credential',async ({page}) => {
+    await page.goto('https://www.saucedemo.com/v1/index.html')
+    await page.locator('#user-name').fill('standard_user')
+    await page.locator('#password').fill('pwd')
+    await page.locator('#login-button').click()
+    const errorText = await page.getByText('Epic sadface: Username and password do not match any user in this service')
+    await expect(errorText).toBeVisible()
+});
