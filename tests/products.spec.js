a267d791807d32589dd00b50c60b7bfdcaa4ca1d
elen <elen.crozzara@gmail.com>
2024-02-17 08:02:58 -0300

    test.fail and step product title verification

--- tests/products.spec.js
+++ tests/products.spec.js
@@ -1,16 +1,23 @@
 import { expect, test } from "@playwright/test";
 
-test('all product names begin with "Sauce Labs"', async ({page}) => {
+test('all product names begin with "Sauce Labs"', async ({ page }) => {
+    test.fail()
     await test.step('login', async () => {
         await page.goto('https://www.saucedemo.com/v1/index.html')
         await expect(await page.title()).toBe('Swag Labs')
         await page.locator('#user-name').fill('standard_user')
         await page.locator('#password').fill('secret_sauce')
         await page.locator('#login-button').click()
-        
-    })
-    await test.step('product title verification', async () => {
 
     })
-    
+    await test.step('product title verification', async () => {
+        const titleListLocator = await page.locator('.inventory_item_name')
+        const productTitleList = await titleListLocator.allTextContents()
+
+        for (const item of productTitleList)
+            await expect(item.slice(0, 10)).toBe('Sauce Labs')
+        // await page.pause()
+
+    })
+
 });
