6ef65e988abd44e919bdb0ed4d43d8a3e870f2bd
elen <elen.crozzara@gmail.com>
2024-02-17 12:05:28 -0300

    add test inventory products

--- tests/products.spec.js
+++ tests/products.spec.js
@@ -8,6 +8,7 @@ test('all product names begin with "Sauce Labs"', async ({ page }) => {
         await page.locator('#user-name').fill('standard_user')
         await page.locator('#password').fill('secret_sauce')
         await page.locator('#login-button').click()
+        await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
 
     })
     await test.step('product title verification', async () => {
@@ -17,7 +18,25 @@ test('all product names begin with "Sauce Labs"', async ({ page }) => {
         for (const item of productTitleList)
             await expect(item.slice(0, 10)).toBe('Sauce Labs')
         // await page.pause()
+    })
+});
 
+test.only('inventory products', async ({page}) => {
+    await test.step('login', async () => {
+        await page.goto('https://www.saucedemo.com/v1/index.html')
+        await expect(await page.title()).toBe('Swag Labs')
+        await page.locator('#user-name').fill('standard_user')
+        await page.locator('#password').fill('secret_sauce')
+        await page.locator('#login-button').click()
+        await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
+    })
+
+    await test.step('validating total products in the inventory', async () => {
+        const inventory = await page.locator('.inventory_item_name')
+        const inventoryListProducts = await inventory.allTextContents()
+
+        for (const item of inventoryListProducts)
+        await expect(inventoryListProducts.length).toBe(6)
     })
 
 });
