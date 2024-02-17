a593c74cf8efe900b8de67e8f4fd6a1131bf0974
elen <elen.crozzara@gmail.com>
2024-02-17 06:57:06 -0300

    creating the product page test

--- tests/products.spec.js
+++ tests/products.spec.js
@@ -1,2 +1,16 @@
 import { expect, test } from "@playwright/test";
 
+test('all product names begin with "Sauce Labs"', async ({page}) => {
+    await test.step('login', async () => {
+        await page.goto('https://www.saucedemo.com/v1/index.html')
+        await expect(await page.title()).toBe('Swag Labs')
+        await page.locator('#user-name').fill('standard_user')
+        await page.locator('#password').fill('secret_sauce')
+        await page.locator('#login-button').click()
+        
+    })
+    await test.step('product title verification', async () => {
+
+    })
+    
+});
