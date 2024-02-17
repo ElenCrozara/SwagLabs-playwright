5aafc1ce56561b66910c185f7df09ae13ad6992b
elen <elen.crozzara@gmail.com>
2024-02-17 12:10:58 -0300

    add describe

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,27 @@
 import { expect, test } from "@playwright/test";
 
 
-test('the user login with succes', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/v1/index.html')
-    await expect(await page.title()).toBe('Swag Labs')
-    await page.locator('#user-name').fill('standard_user')
-    await page.locator('#password').fill('secret_sauce')
-    await page.locator('#login-button').click()
-    await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
-    const productTitle = await page.locator('#inventory_filter_container > div')
-    await expect(productTitle).toHaveText('Products')
-    await page.pause()
-});
+test.describe('login page', async () => {
+
+    test('the user login with succes', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/v1/index.html')
+        await expect(await page.title()).toBe('Swag Labs')
+        await page.locator('#user-name').fill('standard_user')
+        await page.locator('#password').fill('secret_sauce')
+        await page.locator('#login-button').click()
+        await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
+        const productTitle = await page.locator('#inventory_filter_container > div')
+        await expect(productTitle).toHaveText('Products')
+        await page.pause()
+    });
 
 
-test('the user inserts wrong credential',async ({page}) => {
-    await page.goto('https://www.saucedemo.com/v1/index.html')
-    await page.locator('#user-name').fill('standard_user')
-    await page.locator('#password').fill('pwd')
-    await page.locator('#login-button').click()
-    const errorText = await page.getByText('Epic sadface: Username and password do not match any user in this service')
-    await expect(errorText).toBeVisible()
-});
+    test('the user inserts wrong credential', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/v1/index.html')
+        await page.locator('#user-name').fill('standard_user')
+        await page.locator('#password').fill('pwd')
+        await page.locator('#login-button').click()
+        const errorText = await page.getByText('Epic sadface: Username and password do not match any user in this service')
+        await expect(errorText).toBeVisible()
+    });
+})
--- tests/products.spec.js
+++ tests/products.spec.js
@@ -1,42 +1,44 @@
 import { expect, test } from "@playwright/test";
 
-test('all product names begin with "Sauce Labs"', async ({ page }) => {
-    test.fail()
-    await test.step('login', async () => {
-        await page.goto('https://www.saucedemo.com/v1/index.html')
-        await expect(await page.title()).toBe('Swag Labs')
-        await page.locator('#user-name').fill('standard_user')
-        await page.locator('#password').fill('secret_sauce')
-        await page.locator('#login-button').click()
-        await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
+test.describe('products test', async () => {
+    test('all product names begin with "Sauce Labs"', async ({ page }) => {
+        test.fail()
+        await test.step('login', async () => {
+            await page.goto('https://www.saucedemo.com/v1/index.html')
+            await expect(await page.title()).toBe('Swag Labs')
+            await page.locator('#user-name').fill('standard_user')
+            await page.locator('#password').fill('secret_sauce')
+            await page.locator('#login-button').click()
+            await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
 
-    })
-    await test.step('product title verification', async () => {
-        const titleListLocator = await page.locator('.inventory_item_name')
-        const productTitleList = await titleListLocator.allTextContents()
+        })
+        await test.step('product title verification', async () => {
+            const titleListLocator = await page.locator('.inventory_item_name')
+            const productTitleList = await titleListLocator.allTextContents()
 
-        for (const item of productTitleList)
-            await expect(item.slice(0, 10)).toBe('Sauce Labs')
-        // await page.pause()
-    })
-});
+            for (const item of productTitleList)
+                await expect(item.slice(0, 10)).toBe('Sauce Labs')
+            // await page.pause()
+        })
+    });
 
-test.only('inventory products', async ({page}) => {
-    await test.step('login', async () => {
-        await page.goto('https://www.saucedemo.com/v1/index.html')
-        await expect(await page.title()).toBe('Swag Labs')
-        await page.locator('#user-name').fill('standard_user')
-        await page.locator('#password').fill('secret_sauce')
-        await page.locator('#login-button').click()
-        await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
-    })
+    test('inventory products', async ({ page }) => {
+        await test.step('login', async () => {
+            await page.goto('https://www.saucedemo.com/v1/index.html')
+            await expect(await page.title()).toBe('Swag Labs')
+            await page.locator('#user-name').fill('standard_user')
+            await page.locator('#password').fill('secret_sauce')
+            await page.locator('#login-button').click()
+            await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
+        })
 
-    await test.step('validating total products in the inventory', async () => {
-        const inventory = await page.locator('.inventory_item_name')
-        const inventoryListProducts = await inventory.allTextContents()
+        await test.step('validating total products in the inventory', async () => {
+            const inventory = await page.locator('.inventory_item_name')
+            const inventoryListProducts = await inventory.allTextContents()
 
-        for (const item of inventoryListProducts)
-        await expect(inventoryListProducts.length).toBe(6)
-    })
+            for (const item of inventoryListProducts)
+                await expect(inventoryListProducts.length).toBe(6)
+        })
 
-});
+    });
+})
