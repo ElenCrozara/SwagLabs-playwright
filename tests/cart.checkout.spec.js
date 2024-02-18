089756a01eeef90d90e01df6ad602a19b7ee8cd0
elen <elen.crozzara@gmail.com>
2024-02-18 09:39:54 -0300

    refactoring the tests

--- tests/cart.checkout.spec.js
+++ tests/cart.checkout.spec.js
@@ -9,25 +9,39 @@ test.describe('cart test', async () => { //testando o carrinho de compras
             await page.locator('#password').fill('secret_sauce')
             await page.locator('#login-button').click()
             await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
+
         })
         await test.step('select product', async () => {
-            // Localiza a classe '.inventory_item_name' do primeiro produto da lista e clica
+
+            // valida se a lista de produtos esta visivel na página
+            const list = await page.locator('#contents_wrapper')
+            const count = await list.count();
+            const isVisible = await list.isVisible();
+
+            // Localiza a classe do primeiro produto da lista e clica
             await page.locator('.inventory_item_name').nth(0).click();
-            // valida o texto do nome do produto
+            await expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory-item.html?id=4')
             await page.locator('//*[contains(text(),"Sauce Labs Backpac")]').isVisible();
-            // adicionando produto no carrinho clicando no botão ADD TO CART
-            const addToCartButton = await page.locator('.btn_primary.btn_inventory:has-text("ADD TO CART")').click();
+            await expect(page.getByText('29.99')).toBeVisible();
+            await page.getByRole('button', { name: 'ADD TO CART' }).click();
+
         });
-        await test.step('access cart', async () => { // acessa o carrinho depois de selecionar o produto
-            // localizou o elemento do carrinho na página do produto e clicou
+        await test.step('access cart', async () => {
+
+            // clica no elemento do carrinho
             const link = await page.locator('#shopping_cart_container > a').click()
             await expect(page.url()).toMatch('https://www.saucedemo.com/v1/cart.html');
-            // validando se existe o nome do produto na página do carrinho
-            await page.locator('a#item_4_title_link').textContent('Sauce Labs Backpack')
-            // validando se existe o preço
-            await page.locator('.item_pricebar').textContent('29.99')
-            // clica no botão checkout
-            await page.locator('a.btn_action.checkout_button').click()
+            await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
+            await expect(page.getByText('29.99')).toBeVisible();
+            await page.dispatchEvent('a.btn_action.checkout_button', 'click');
+            await page.waitForURL('https://www.saucedemo.com/v1/checkout-step-one.html');
+
+            // await page.waitForSelector('a.btn_action.checkout_button');
+            // await page.click('a.btn_action.checkout_button');
+            // await page.getByRole('button', { name: 'CHECKOUT ' }).click();
+            // const addToCartButton = await page.locator('.btn_primary.btn_inventory:has-text("ADD TO CART")').click();
+
+            
         })
         // criar step para testar página checkout
     });
